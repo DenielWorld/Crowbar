c63a37038e33a1b5c0f1f99902b1592c3d1cfead
DenielW <denielmirzoyan@gmail.com>
2019-08-06 18:43:09 -0700

    Release 1/2?
    I was just bored so I made this plugin :p

---
+++ src/DenielWorld/Main.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace DenielWorld;
+
+use pocketmine\block\Block;
+use pocketmine\block\BlockFactory;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\item\Item;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->reloadConfig();
+        if(!file_exists("config.yml")){
+            $this->saveResource("config.yml");
+        }
+    }
+
+    public function translateText($string, $blockname, $itemname, $playername){
+        $msg = str_replace("{block}", $blockname, $string);
+        $msg = str_replace("{item}", $itemname, $msg);
+        $msg = str_replace("{player}", $playername, $msg);
+        return $msg;
+    }
+
+    public function onHold(PlayerItemHeldEvent $event){
+        $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $player = $event->getPlayer();
+        $inv = $player->getInventory();
+        $item = $inv->getItemInHand();
+        if($item->getId() == $cfg->get("crowbar-id") and $item->getDamage() == $cfg->get("crowbar-meta")) {
+            $item->setCustomName(TextFormat::colorize($cfg->get("crowbar-name")));
+            $item->setLore([TextFormat::colorize($cfg->get("crowbar-lore-1")), TextFormat::colorize($cfg->get("crowbar-lore-2")), TextFormat::colorize($cfg->get("crowbar-lore-3")), TextFormat::colorize($cfg->get("crowbar-lore-4"))]);
+            $inv->setItem($inv->getHeldItemIndex(), $item);
+        }
+    }
+
+    public function onInteract(PlayerInteractEvent $event){
+        $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $player = $event->getPlayer();
+        $inv = $player->getInventory();
+        $block = $event->getBlock();
+        $item = $inv->getItemInHand();
+        if($item->getId() == $cfg->get("crowbar-id") and $item->getDamage() == $cfg->get("crowbar-meta")){
+            $blockname = $block->getName();
+            $itemname = $item->getName();
+            $playername = $player->getName();
+            $blockitem = $block->getPickedItem();
+            $blockitem->setCustomName($block->getName());
+            $blockitem->setDamage($block->getDamage());
+            $air = new Item(0);
+            $player->sendMessage($this->translateText(TextFormat::colorize($cfg->get("broken-block-message")), $blockname, $itemname, $playername));
+            $block->getLevel()->dropItem($block->asVector3(), $blockitem);
+            $block->getLevel()->setBlock($block->asVector3(), BlockFactory::get(Block::AIR));
+            $inv->setItem($inv->getHeldItemIndex(), $air);
+        }
+    }
+
+    public function onDisable()
+    {
+        $this->saveDefaultConfig();
+    }
+}
